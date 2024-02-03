5b2cabf280555af1b4ca5bc3015f7b8fe7cd0174
Deepak <60600822+dapacks@users.noreply.github.com>
2024-02-04 02:10:28 +0530

    Update Home.jsx

--- yumyum/src/screens/Home.jsx
+++ yumyum/src/screens/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [foodItem, setFoodItem] = useState([]);
 
   const loadData = async () => {
-    let response = await fetch("http://localhost:5000/api/foodData", {
+    let response = await fetch("https://practicebackend-five.vercel.app/api/foodData", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
