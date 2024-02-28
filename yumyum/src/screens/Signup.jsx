d121d372f62eae20fb48102defc7e904b62d18db
Deepak <60600822+dapacks@users.noreply.github.com>
2024-02-28 12:54:28 +0530

    Update Signup.jsx

--- yumyum/src/screens/Signup.jsx
+++ yumyum/src/screens/Signup.jsx
@@ -8,7 +8,7 @@ const handleSubmit=async (e)=>
 {
   e.preventDefault();
   console.log(JSON.stringify({name:credentials.name,email:credentials.email,password:credentials.password,location:credentials.geolocation}));
-  const response=await fetch("https://practicebackend-five.vercel.app/api/createuser",{
+  const response=await fetch("https://deepak-fqpy.onrender.com//api/createuser",{
     method:'POST',
     headers:{
       'Content-type':'application/json'
