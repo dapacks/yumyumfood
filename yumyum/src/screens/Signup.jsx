1afaae096defc624c26d8d08653d14ff1a1eddc5
Deepak <60600822+dapacks@users.noreply.github.com>
2024-03-28 21:30:38 +0530

    Update Signup.jsx

--- yumyum/src/screens/Signup.jsx
+++ yumyum/src/screens/Signup.jsx
@@ -42,6 +42,43 @@ const onChange = (e) => {
 
   return (
     <>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+  <div className="container-fluid">
+    <Link className="navbar-brand fs-1 fst-italic" to="/">YumYum</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul className="navbar-nav me-auto mb-2">
+       
+       {(localStorage.getItem("authToken")) ?   <li> <Link className="nav-link active fs-5" aria-current="page" to="/myOrder">My Orders</Link>
+       </li>:"" }
+      </ul>
+
+      {(!localStorage.getItem("authToken"))?
+      <div className='d-flex'>
+      <Link className="btn bg-white text-success mx-1" to="/login">Login</Link>
+        <Link className="btn bg-white text-success mx-1" to="/createuser">Signup</Link>
+      </div> :
+       <div>
+        <div className='btn bg-white text-success mx-2' onClick={()=>{setcartview(true)}} >My Cart {' '}
+        <Badge pill bg="danger">{cartlength.length > 0 ? cartlength.length : ""}</Badge>
+        </div>
+        {cartview ? <Modal onClose={()=>setcartview(false)}><Cart/></Modal>:null}
+        <div className='btn bg-white text-danger mx-2' onClick={handleLogout} >Logout</div>
+       </div>
+
+      
+      }
+    </div>
+  </div>
+</nav>
+    </div>
+
+
+
+
      <div className="container">
     <form onSubmit={handleSubmit}>
        <div className="mb-3">
@@ -77,7 +114,7 @@ const onChange = (e) => {
    
    
     <button type="submit" className="btn btn-success">Submit</button>
-    <Link to="/login" className='m-3 btn btn-danger'>registered user</Link>
+    <Link to="/login" className='m-3 btn btn-danger'>Already a user</Link>
   </form>
   </div>
   </>
