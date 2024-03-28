c16b091780a9abea50ec2c1055f657946a3d094a
Deepak <60600822+dapacks@users.noreply.github.com>
2024-03-28 21:27:28 +0530

    Update Login.jsx

--- yumyum/src/screens/Login.jsx
+++ yumyum/src/screens/Login.jsx
@@ -12,7 +12,7 @@ let navigate=useNavigate()
         password: credentials.password,
       })
     );
-    const response = await fetch("https://deepak-fqpy.onrender.com/api/loginuser", {
+    const response = await fetch("http://localhost:5000/api/loginuser", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
@@ -46,7 +46,41 @@ let navigate=useNavigate()
   };
 
   return (
+
+      <>
+       <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+  <div className="container-fluid">
+    <Link className="navbar-brand fs-1 fst-italic" to="/">YumYum</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul className="navbar-nav me-auto mb-2">
+       
+       {(localStorage.getItem("authToken")) ?   <li> <Link className="nav-link active fs-5" aria-current="page" to="/myOrder">My Orders</Link>
+       </li>:"" }
+      </ul>
+
+      {(!localStorage.getItem("authToken"))?
+      <div className='d-flex'>
+      <Link className="btn bg-white text-success mx-1" to="/login">Login</Link>
+        <Link className="btn bg-white text-success mx-1" to="/createuser">Signup</Link>
+      </div> :
+       <div>
+        <div className='btn bg-white text-success mx-2' onClick={()=>{setcartview(true)}} >My Cart {' '}
+        <Badge pill bg="danger">{cartlength.length > 0 ? cartlength.length : ""}</Badge>
+        </div>
+        {cartview ? <Modal onClose={()=>setcartview(false)}><Cart/></Modal>:null}
+        <div className='btn bg-white text-danger mx-2' onClick={handleLogout} >Logout</div>
+       </div>
+
       
+      }
+    </div>
+  </div>
+</nav>
+    </div>
         <div className="container">
           <form onSubmit={handleSubmit}>
             <div className="mb-3">
@@ -84,7 +118,7 @@ let navigate=useNavigate()
             </Link>
           </form>
         </div>
-        
+        </>
       
       )
    
